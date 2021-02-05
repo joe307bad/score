67d2d5aaec81da6e9e7b962855e98561cec0e14b
Joe Badaczewski <jbadaczewski@softwriters.com>
2021-02-05 01:38:51 -0500

    mobile - monorepo working with code sharing

---
+++ packages/mobile/App.tsx
@@ -0,0 +1,7 @@
+/**
+ * Do not modify this file - it is a proxy to your `App.re` file
+ * located in the `src/` folder.
+ */
+import { meaningOfLife } from '@score/foo';
+console.log(meaningOfLife);
+export { app as default } from "./lib/es6/src/App.bs.js";
